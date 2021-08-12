134885f6a90b97213ceceda88fa7bf431754cc65
Anant Rawat <anant.rawat@sunfox.in>
2021-08-12 08:35:57 +0530

    Lesson 8 started

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/layout/Navbar";
 import Dashboard from "./Components/dashboard/Dashboard";
+import ProjectDetails from "./Components/projects/ProjectDetails";
+import SignIn from "./Components/auth/SignIn";
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +11,8 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" component={Dashboard} />
+          <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
         </Switch>
       </div>
     </BrowserRouter>
