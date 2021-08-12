5dac83d7b2fc1aab837c63a44980c1db614847d4
Anant Rawat <anant.rawat@sunfox.in>
2021-08-12 08:52:30 +0530

    Project Details updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/layout/Navbar";
 import Dashboard from "./Components/dashboard/Dashboard";
+import ProjectDetails from "./Components/projects/ProjectDetails";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
         <Switch>
-          <Route path="/" component={Dashboard} />
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/project/:id" component={ProjectDetails} />
         </Switch>
       </div>
     </BrowserRouter>
