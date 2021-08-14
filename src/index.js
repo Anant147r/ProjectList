28ad1fea1975677f1dd13206d13877cbdd4df48c
Anant Rawat <anant.rawat@sunfox.in>
2021-08-14 09:55:36 +0530

    Thunk added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
-const store = createStore(rootReducer);
+import thunk from "redux-thunk";
+import { applyMiddleware } from "redux";
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
